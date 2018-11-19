239a10f079e00d4328c117094f8aacfbd410b1ac
Martin Herrmann <martin.herrmann@uni-ulm.de>
2018-11-19 12:12:52 +0100

    [ADD] Config.cmake file for finding the package in other build envs

---
+++ aduulm_cmake_toolsConfig.cmake
@@ -0,0 +1,18 @@
+cmake_minimum_required(VERSION 2.8.11 FATAL_ERROR)
+
+if ( aduulm_cmake_tools_FOUND )
+  return()
+endif()
+
+#
+# Versioning
+#
+
+set(aduulm_cmake_tools_VERSION_MAJOR 0)
+set(aduulm_cmake_tools_VERSION_MINOR 0)
+set(aduulm_cmake_tools_VERSION_PATCH 1)
+set(aduulm_cmake_tools_VERSION "${aduulm_cmake_tools_VERSION_MAJOR}.${aduulm_cmake_tools_VERSION_MINOR}.${aduulm_cmake_tools_VERSION_PATCH}")
+message(STATUS "Using aduulm_cmake_tools, Version ${aduulm_cmake_tools_VERSION}")
+
+#configure_file(${CMAKE_CURRENT_LIST_DIR}/cmake/aduulm_cmake_tools-extras.cmake.em ${CMAKE_BINARY_DIR}/aduulm_cmake_tools-extras.cmake)
+include(${CMAKE_BINARY_DIR}/aduulm_cmake_tools-extras.cmake)
