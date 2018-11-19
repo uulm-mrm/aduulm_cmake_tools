59b9a40851c27fe826d39ce4abeace95c5cd5370
Jan Strohbeck <jan.strohbeck@uni-ulm.de>
2018-11-19 12:39:46 +0100

    [UDPATE] Use standard configure_file instead of empy.

--- aduulm_cmake_toolsConfig.cmake
+++ aduulm_cmake_toolsConfig.cmake
@@ -1,6 +1,6 @@
 cmake_minimum_required(VERSION 2.8.11 FATAL_ERROR)
 
-if ( aduulm_cmake_tools_FOUND )
+if(aduulm_cmake_tools_FOUND)
   return()
 endif()
 
@@ -14,5 +14,9 @@ set(aduulm_cmake_tools_VERSION_PATCH 1)
 set(aduulm_cmake_tools_VERSION "${aduulm_cmake_tools_VERSION_MAJOR}.${aduulm_cmake_tools_VERSION_MINOR}.${aduulm_cmake_tools_VERSION_PATCH}")
 message(STATUS "Using aduulm_cmake_tools, Version ${aduulm_cmake_tools_VERSION}")
 
-#configure_file(${CMAKE_CURRENT_LIST_DIR}/cmake/aduulm_cmake_tools-extras.cmake.em ${CMAKE_BINARY_DIR}/aduulm_cmake_tools-extras.cmake)
-include(${CMAKE_BINARY_DIR}/aduulm_cmake_tools-extras.cmake)
+configure_file(
+  ${CMAKE_CURRENT_LIST_DIR}/cmake/aduulm_cmake_tools-extras.cmake.in
+  ${CMAKE_CURRENT_BINARY_DIR}/aduulm_cmake_tools-extras.cmake
+  @ONLY
+)
+include(${CMAKE_CURRENT_BINARY_DIR}/aduulm_cmake_tools-extras.cmake)
